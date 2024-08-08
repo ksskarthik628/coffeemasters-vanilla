bc38e8943d0945e87aba175b5ef91737e8baa550
Karthik Kaipa <ksskarthik628@gmail.com>
2024-08-08 17:46:35 -0600

    Second session/VanillaJS is high performance but a pain in the butt

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 import Store from './services/Store.js';
+import Router from './services/Router.js';
 import { loadData } from './services/Menu.js';
+
+// Link my web components
+import MenuPage from './components/MenuPage.js';
+import DetailsPage from './components/DetailsPage.js';
+import OrderPage from './components/OrderPage.js';
+import ProductItem from './components/ProductItem.js';
+import CartItem from './components/CartItem.js';
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener('DOMContentLoaded', () => {
     loadData();
+    app.router.init();
+});
+
+window.addEventListener('appcartchange', (event) => {
+    const badge = document.getElementById('badge');
+    const quantity = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+    badge.textContent = quantity;
+    badge.hidden = quantity == 0;
 });
